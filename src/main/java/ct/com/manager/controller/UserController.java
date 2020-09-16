7dbac1d1e79384c8a52e80f56d8cc0f0bacf8542
user <643379347@qq.com>
2020-09-16 17:34:00 +0800

    静态资源无法生效

--- src/main/java/ct/com/manager/controller/UserController.java
+++ src/main/java/ct/com/manager/controller/UserController.java
@@ -7,23 +7,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserController {
+    ModelAndView modelAndView = new ModelAndView();
     @Autowired
     UserService userService;
+
     //登录用户
     @RequestMapping("/user/login")
     @ResponseBody
-    public String login(String username,String password){
-            User user=userService.login(username,password);
-            if(user!=null) {
-                System.out.println(user.getUsername());
-                return "/index.html";
-            }else{
-               return "/login.html" ;
-            }
+    public ModelAndView login(String username, String password) {
+        User user = userService.login(username, password);
+        if (user != null) {
+            modelAndView.setViewName("index");
+            return modelAndView;
+        } else {
+            modelAndView.setViewName("login");
+            return modelAndView;
+        }
     }
+
     //注册用户
     @RequestMapping("/user/register")
     @ResponseBody
@@ -32,4 +37,11 @@ public class UserController {
         return "login";
     }
 
+    @RequestMapping("/user/test")
+    @ResponseBody
+    public ModelAndView test() {
+        modelAndView.setViewName("login");
+        return modelAndView;
+
+    }
 }
